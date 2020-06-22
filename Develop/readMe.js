3ae4f1e7a46c12ec91bc7429f724aa4ab95beaab
Steve Munoz <stevomunoz@yahoo.com>
2020-06-21 21:04:23 -0500

    setup files and created variables and wrote out the questions

--- Develop/readMe.js
+++ Develop/readMe.js
@@ -1,5 +1,55 @@
-function writeToFile(fileName, data) {}
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
 
-function init() {}
+const writeFileAsync = util.promisify(fs.writeFile);
 
-init();
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a brief description of the proejct",
+    },
+    {
+      type: "editor",
+      name: "table",
+      message: "Write table of contents?",
+    },
+    {
+      type: "editor",
+      name: "installation",
+      message: "What is the installation process?",
+    },
+    {
+      type: "editor",
+      name: "usage",
+      message: "What is this used for?",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Are there any license?",
+    },
+    {
+      type: "editor",
+      name: "contributing",
+      message: "Who is contributing?",
+    },
+    {
+      type: "editor",
+      name: "tests",
+      message: "Type out any tests done",
+    },
+    {
+      type: "Editor",
+      name: "questions",
+      message: "Questions?",
+    },
+  ]);
+}
