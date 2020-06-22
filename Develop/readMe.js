2e2fc49a97ae1f768af33a47095e450327ce0cb4
Steve Munoz <stevomunoz@yahoo.com>
2020-06-22 10:37:14 -0500

    using generateMarkdown.js I created a function to return my readme information by using back tics and string interpolation, I also exported this JS file so that I can use it in my readMe.js where I have created a prompt to ask user questions

--- Develop/readMe.js
+++ Develop/readMe.js
@@ -1,55 +1,71 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
-const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
-const writeFileAsync = util.promisify(fs.writeFile);
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "project title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "project description:",
+  },
+  {
+    type: "input",
+    name: "badges",
+    message: "project badges:",
+  },
+  {
+    type: "editor",
+    name: "installation",
+    message: "installation steps:",
+  },
+  {
+    type: "editor",
+    name: "usage",
+    message: "instructions:",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "project license: (ISC)",
+    default: "ISC",
+  },
+  {
+    type: "editor",
+    name: "contributing",
+    message: "contributing instructions:",
+  },
+  {
+    type: "editor",
+    name: "tests",
+    message: "project tests:",
+  },
+  {
+    type: "input",
+    name: "profile_picture",
+    message: "profile picture url:",
+  },
+  {
+    type: "input",
+    name: "gh_email",
+    message: "GitHub email:",
+  },
+];
 
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is your title?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Provide a brief description of the proejct",
-    },
-    {
-      type: "editor",
-      name: "table",
-      message: "Write table of contents?",
-    },
-    {
-      type: "editor",
-      name: "installation",
-      message: "What is the installation process?",
-    },
-    {
-      type: "editor",
-      name: "usage",
-      message: "What is this used for?",
-    },
-    {
-      type: "input",
-      name: "license",
-      message: "Are there any license?",
-    },
-    {
-      type: "editor",
-      name: "contributing",
-      message: "Who is contributing?",
-    },
-    {
-      type: "editor",
-      name: "tests",
-      message: "Type out any tests done",
-    },
-    {
-      type: "Editor",
-      name: "questions",
-      message: "Questions?",
-    },
-  ]);
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (error) => {
+    if (error) console.log("Can't save file due to error: " + error);
+  });
 }
+
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", generateMarkdown(data));
+  });
+}
+
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,36 @@
 function generateMarkdown(data) {
   return `
+  
+${data.badges}
 # ${data.title}
-
+## Table of Contents
+- ${data.title}
+- Description
+- Installation
+- Usage
+- License
+- Contributing
+- Tests
+- Questions
+  - ${data.gh_email}
+  - ${data.gh_email}
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${data.license}
+## Contributing
+${data.contributing}
+## Tests
+${data.tests}
+## Questions
+### User GitHub profile picture
+- ${data.profile_picture}
+### User GitHub email
+- ${data.gh_email}
 `;
 }
 
